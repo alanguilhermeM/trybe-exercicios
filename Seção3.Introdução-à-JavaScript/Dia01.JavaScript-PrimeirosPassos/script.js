cddb2a8efcc1eaccd6328fb5ded6d1b2ba2a8ea1
alanguilhermeM <alanguilherme13@outlook.com>
2023-01-23 15:01:10 -0300

    Utilizando Condições if e else

--- Seção3.Introdução-à-JavaScript/Dia01.JavaScript-PrimeirosPassos/script.js
+++ Seção3.Introdução-à-JavaScript/Dia01.JavaScript-PrimeirosPassos/script.js
@@ -11,11 +11,24 @@
 
 
 
-const base = 5;
-const height = 8;
-const area = base * height;
+// const base = 5;
+// const height = 8;
+// const area = base * height;
 
-console.log(area);
+// console.log(area);
 
-const perimeter = base + (height+height);
-console.log(perimeter);
+// const perimeter = base + (height+height);
+// console.log(perimeter);
+
+
+
+let nota = 50;
+if (nota >= 80) {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else if (nota < 60) {
+    console.log("Infelizmente, você foi reprovado.");
+}
