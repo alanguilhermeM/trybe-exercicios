282a86e4a07e3216e840b0d9a7b68847538c6e34
alanguilhermeM <alanguilherme13@outlook.com>
2023-01-23 13:36:22 -0300

    Criando constantes de base e utilizando soma e multiplicação

--- Seção3.Introdução-à-JavaScript/Dia01.JavaScript-PrimeirosPassos/script.js
+++ Seção3.Introdução-à-JavaScript/Dia01.JavaScript-PrimeirosPassos/script.js
@@ -1,5 +1,21 @@
-const myName = "Guilherme";
-const birthCity = "São João Del Rei";
-let birthYear = 2001;
+// const myName = "Guilherme";
+// const birthCity = "São João Del Rei";
+// let birthYear = 2001;
 
-console.log(myName, birthCity, birthYear);
+//    console.log(myName, birthCity, birthYear);
+
+//    birthYear = 2030;
+//    console.log(birthYear);
+//    birthCity = "Tiradentes";
+//    console.log(birthCity);
+
+
+
+const base = 5;
+const height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = base + (height+height);
+console.log(perimeter);
