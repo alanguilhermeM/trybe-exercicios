4f5c401ba2a0000fb0b03dbf0fc6696edbcc320b
alanguilhermeM <alanguilherme13@outlook.com>
2023-01-29 12:52:20 -0300

    Bonus

---
+++ Seção3.Introdução-à-JavaScript/Dia03.Logica-de-Programação/ExercicioBonus.js
@@ -0,0 +1,46 @@
+// Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+
+// n = 5
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// let n = 5;
+// let symbol = '*';
+// let inputline = '';
+// if (n > 1) {
+//     for (let i = 0; i < n; i += 1) {
+//         inputline += symbol
+//     }
+//     for (let i = 0; i < n; i += 1) {
+//         console.log(inputline);
+//     }
+// }
+// Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+// let n = 5;
+// let symbol = '*';
+// let inputline = '';
+// if (n > 1) {
+//     for (let i = 0; i < n; i += 1) {
+//         inputline += symbol
+//         console.log(inputline);
+//     }
+// }
+
+
+// Agora inverta o lado do triângulo. 
+
+
+let n = 5;
+let symbol = '*';
+let inputline = '';
+if (n > 1) {
+    for (let i = 0; i < n; i += 1) {
+        inputline += symbol
+        console.log(inputline);
+    }
+}
