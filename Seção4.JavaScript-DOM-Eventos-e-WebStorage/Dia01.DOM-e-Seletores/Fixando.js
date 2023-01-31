65e3ab3c0314f73f02e79f634ec60614e21cad03
alanguilhermeM <alanguilherme13@outlook.com>
2023-01-31 12:29:44 -0300

    Fixando conteudo

---
+++ Seção4.JavaScript-DOM-Eventos-e-WebStorage/Dia01.DOM-e-Seletores/Fixando.js
@@ -0,0 +1,22 @@
+let header = document.getElementById('header-container');
+header.style.backgroundColor = 'green';
+
+let emergengy = document.getElementsByClassName('emergency-tasks')[0];
+emergengy.style.backgroundColor = 'pink';
+
+let importante = document.getElementsByTagName('h3')[0];
+importante.style.backgroundColor = 'purple';
+let importante1 = document.getElementsByTagName('h3')[1];
+importante1.style.backgroundColor = 'purple';
+
+
+let noEmergengy = document.getElementsByClassName('no-emergency-tasks')[0];
+noEmergengy.style.backgroundColor = 'yellow';
+
+let importante2 = document.getElementsByTagName('h3')[2];
+importante2.style.backgroundColor = 'black';
+let importante3 = document.getElementsByTagName('h3')[3];
+importante3.style.backgroundColor = 'black';
+
+let footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'Darkgreen'
